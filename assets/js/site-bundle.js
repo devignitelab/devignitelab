2d96f393d632c7bc4493d99fdad9572d28269d1b
devignitelab <135448014+devignitelab@users.noreply.github.com>
2023-06-04 17:28:47 +0530

    move js files and bundle

---
+++ assets/js/site-bundle.js
@@ -0,0 +1,8 @@
+---
+layout: null
+---
+{% include /assets/js/app.js %};
+{% include /assets/js/menu.js %};
+{% include /assets/js/lunr.min.js %};
+{% include /assets/js/search.js %};
+{% include /assets/js/custom.js %};
